8f72caf0839e5148e1dbae92140c66d5f14f1412
EdwardUA <32891570+EdwardUA@users.noreply.github.com>
2018-03-18 14:23:58 +0200

    Add files via upload

---
+++ 10 03 18 spisok s dvoinoi svyazy/dvosvyazniy spisok/src/ListElement.java
@@ -0,0 +1,5 @@
+
+public class ListElement {
+	ListElement next;    
+	int data;  
+}
---
+++ 10 03 18 spisok s dvoinoi svyazy/dvosvyazniy spisok/src/Run.java
@@ -0,0 +1,11 @@
+
+public class Run {
+	public static void main(String[] args) {
+		List list = new List();	
+		
+		list.addFirst(1);
+		list.addFirst(2);
+		list.addLast(3);
+		list.printList();
+}
+}
